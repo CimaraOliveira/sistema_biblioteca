7f7f08affa40173f964e284bf562f93704cd1dbe
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2022-04-08 15:24:28 -0300

    adicionando filtros de busca no admin

--- livro/admin.py
+++ livro/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Livro
 
 class LivroAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'autor','isbn']
+    list_display = ['nome', 'autor','isbn', 'editora'] #visualizar
+    list_filter = ['nome', 'autor'] #filtrando por nome e autor
+    list_display_links = ['nome'] #clicavel
+    list_per_page = 2 #paginação
+    search_fields = ['nome', 'autor'] #pesquisa
 
 
 
